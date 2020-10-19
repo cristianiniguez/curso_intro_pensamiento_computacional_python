fb830f437c1fba57fd8fc81901f5378fb01debb4
criniguez <felizpc1@gmail.com>
2020-10-19 14:06:07 -0400

    introduccion: elementos basicos

---
+++ introduccion/elementos_basicos_python.py
@@ -0,0 +1,20 @@
+# Obteniendo el tipo de objeto
+
+my_int = 1
+my_float = 1.0
+my_bool = False
+my_none = None
+
+type(my_int)  # <class 'int'>
+type(my_float)  # <class 'float'>
+type(my_bool)  # <class 'bool'>
+type(my_none)  # <class 'NoneType'>
+
+1 + 2  # 3
+2 - 5  # -3
+2.0 * 3  # 6.0
+6 // 2  # 3
+6 // 4  # 1
+6 / 4  # 1.5
+7 % 2  # 1
+2**3  # 8
