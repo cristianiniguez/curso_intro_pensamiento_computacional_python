98dbfe38ff938eb3570d6052ffc7853e73ab5f0d
criniguez <felizpc1@gmail.com>
2020-10-22 13:45:57 -0400

    funciones: recursividad

---
+++ funciones_alcance_abstraccion/factoriales.py
@@ -0,0 +1,17 @@
+# Hallar el factorial de un número
+
+def factorial(n):
+    """Calcula el factorial de n.
+
+    n int > 0
+    return n!
+    """
+    print(n)
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+n = int(input('Escribe un entrero: '))
+
+print(factorial(n))
