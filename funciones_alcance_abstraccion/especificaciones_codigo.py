61502bd4d924e767e6c23167581668c24bb0e15e
criniguez <felizpc1@gmail.com>
2020-10-22 13:28:09 -0400

    funciones: especificaciones del codigo

---
+++ funciones_alcance_abstraccion/especificaciones_codigo.py
@@ -0,0 +1,16 @@
+def a(cualquier_parametro):
+    """
+    Descripción de lo que hace nuestra función
+    cualquier_parametro int cualquier entero
+    return cualquier_parametro + 5
+    """
+    return cualquier_parametro + 5
+
+
+help(a)
+'''
+a(cualquier_parametro)
+    Descripción de lo que hace nuestra función
+    cualquier_parametro int cualquier entero
+    return cualquier_parametro + 5
+'''
